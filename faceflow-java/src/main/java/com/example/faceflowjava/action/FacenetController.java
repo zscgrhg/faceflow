ec5abf334e16cf454cee1b8c624cb78c4c617672
zscgrhg <zscgrhg@qq.com>
2018-12-09 14:50:27 +0800

    引入hbase-client

---
+++ faceflow-java/src/main/java/com/example/faceflowjava/action/FacenetController.java
@@ -0,0 +1,95 @@
+package com.example.faceflowjava.action;
+
+import com.example.faceflowjava.beandefs.HBaseClient;
+import com.example.faceflowjava.grpc.FaceFlowClient;
+import com.example.facematrix.FaceEntry;
+import com.example.facematrix.FaceMatrix;
+import com.google.protobuf.ByteString;
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.constraints.NotBlank;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/faces")
+public class FacenetController {
+    @Autowired
+    FaceFlowClient faceFlowClient;
+    @Autowired
+    HBaseClient hBaseClient;
+
+    private static double[] unwrap(FaceMatrix.Matrix matrix) {
+        return matrix
+                .getMatrixList()
+                .stream()
+                .mapToDouble(Double::doubleValue)
+                .toArray();
+    }
+
+    private static double[] unwrap(FaceEntry.Entry matrix) {
+        return matrix
+                .getMatrixList()
+                .stream()
+                .mapToDouble(Double::doubleValue)
+                .toArray();
+    }
+
+    @RequestMapping("/index")
+    public String index(Map model) {
+        model.putIfAbsent("message", "Facenet meets Tensorflow!");
+        model.putIfAbsent("matches", "");
+        return "faces/index";
+    }
+
+    @RequestMapping("/add")
+    public String add(@RequestParam("face1") MultipartFile face1,
+                      @NotBlank @RequestParam(value = "employId", required = true) String employId,
+                      RedirectAttributes redirectAttributes) throws IOException {
+        byte[] bytes = face1.getBytes();
+        FaceMatrix.Matrix matrix1 = getMatrix(bytes);
+
+        String tstamp = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date());
+
+        hBaseClient.addFace(employId, tstamp, bytes, matrix1);
+
+
+        redirectAttributes.addFlashAttribute("message",
+                "successfully!");
+        return "redirect:/faces/index";
+    }
+
+    @RequestMapping("/identify")
+    public String identify(@RequestParam("face") MultipartFile face,
+                           @NotBlank @RequestParam(value = "employId", required = true) String employId,
+                           RedirectAttributes redirectAttributes) throws IOException {
+        FaceMatrix.Matrix matrix = getMatrix(face.getBytes());
+        List<FaceMatrix.Matrix> matrices = hBaseClient.find(employId);
+        boolean matches = matrices
+                .stream()
+                .allMatch(t -> {
+                    double compute = new EuclideanDistance().compute(
+                            unwrap(t), unwrap(matrix));
+                    return compute < 1.0;
+                });
+        redirectAttributes.addFlashAttribute("matches", matches);
+        return "redirect:/faces/index";
+    }
+
+    private FaceMatrix.Matrix getMatrix(byte[] file) {
+        FaceMatrix.Face face = FaceMatrix.Face
+                .newBuilder()
+                .setFace(ByteString.copyFrom(file))
+                .build();
+        return faceFlowClient.getMatrix(face);
+    }
+}
--- faceflow-java/src/main/java/com/example/faceflowjava/beandefs/HBaseClient.java
+++ faceflow-java/src/main/java/com/example/faceflowjava/beandefs/HBaseClient.java
@@ -1,14 +1,20 @@
 package com.example.faceflowjava.beandefs;
 
+import com.example.facematrix.FaceMatrix;
+import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.experimental.Delegate;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.stream.Collectors;
 
 @Component
 public class HBaseClient implements DisposableBean, Connection {
@@ -18,10 +24,78 @@ public class HBaseClient implements DisposableBean, Connection {
     public final Connection create() {
         try {
             Configuration conf = HBaseConfiguration.create();
-            return ConnectionFactory.createConnection(conf);
+            Connection connection = ConnectionFactory.createConnection(conf);
+            initTables(connection);
+            return connection;
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
+        }
+    }
+
+    public void addFace(String employId,
+                        String id,
+                        byte[] face,
+                        FaceMatrix.Matrix matrix) throws IOException {
+        Table table = connection.getTable(TableName.valueOf("default:facesmatrix"));
+        Put putObj = new Put(employId.getBytes());
+        //上面行指定完后,就需要指定列族,列,值,这样才是完整的
+        putObj.addColumn("face".getBytes(), id.getBytes(),
+                face);
+        putObj.addColumn("matrix".getBytes(),
+                id.getBytes(),
+                matrix.toByteArray());
+        //执行~
+        table.put(putObj);
+    }
+
+    public List<FaceMatrix.Matrix> find(String employId) throws IOException {
+        Table table = connection.getTable(TableName.valueOf("default:facesmatrix"));
+        Get get = new Get(employId.getBytes());
+        get.addFamily("matrix".getBytes());
+        Result result = table.get(get);
+        NavigableMap<byte[], byte[]> familyMap =
+                result.getFamilyMap("matrix".getBytes());
+        List<FaceMatrix.Matrix> matrices = familyMap
+                .values()
+                .stream()
+                .map(this::parse)
+                .collect(Collectors.toList());
+        return matrices;
+    }
+
+    private FaceMatrix.Matrix parse(byte[] data) {
+        try {
+            return FaceMatrix.Matrix.parseFrom(data);
+        } catch (InvalidProtocolBufferException e) {
+            return null;
+        }
+    }
+
+    private void initTables(Connection connection) throws IOException {
+        Admin admin = connection.getAdmin();
+        //创建表名描述
+        TableName tableName = TableName.valueOf("default:facesmatrix");
+        //创建表描述,并赋予表名
+        TableDescriptorBuilder tableBuilder = TableDescriptorBuilder
+                .newBuilder(tableName);
+        //Builder构建列族信息
+        ColumnFamilyDescriptor faceid = ColumnFamilyDescriptorBuilder
+                .newBuilder("face".getBytes())
+                .setInMemory(false)
+                .build();
+        ColumnFamilyDescriptor matrixid = ColumnFamilyDescriptorBuilder
+                .newBuilder("matrix".getBytes())
+                .setInMemory(true)
+                .build();
+        //将列族信息也赋予表描述对象
+        TableDescriptor table = tableBuilder
+                .setColumnFamilies(Arrays.asList(faceid, matrixid))
+                .build();
+        //创建表
+        boolean tableExists = admin.tableExists(tableName);
+        if (!tableExists) {
+            admin.createTable(table);
         }
     }
 
